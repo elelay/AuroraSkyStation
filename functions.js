4febb0b35cf361c41f00c5813c5f01fd3c976f24
Eric Le Lay <elelay@macports.org>
2015-11-09 19:18:19 +0100

    more predefs + add object def

--- functions.js
+++ functions.js
@@ -66,11 +66,36 @@ var predefPrototypes = {
         upsert: 4,
         // https://github.com/meteorhacks/meteor-aggregate
         aggregate: 2
+    },
+    // Tracker.Dependency functions, from docs.meteor.com
+    "Tracker.Dependency": {
+        client: {
+            changed: 0,
+            depend: 1,
+            hasDependent: 0
+        }
+    },
+    // ReactiveVar functions, from docs.meteor.com
+    "ReactiveVar": {
+        client: {
+            get: 0,
+            set: 1
+        }
+    },
+    // Logger, from jag:pince
+    "Logger": {
+        trace: 10000,
+        debug: 10000,
+        info: 10000,
+        warn: 10000
     }
 };
 
 var predefObjects = {
     "Presences": {
+        lib: "Mongo.Collection"
+    },
+    "Meteor.users": {
         lib: "Mongo.Collection"
     }
 };
@@ -116,6 +141,32 @@ var predefs = {
             getView: 2
         }
     },
+    // https://atmospherejs.com/meteor/browser-policy
+    "BrowserPolicy": {
+        server: {
+            content: -1,
+            framing: -1
+        }
+    },
+    // https://github.com/meteorhacks/cluster
+    "Cluster": {
+        lib: {
+            connect: 2,
+            discoverConnection: 1,
+            register: 2
+        }
+    },
+    "DDP": {
+        lib: {
+            connect: 1
+        }
+    },
+    // https://nodejs.org/api/fs.html
+    "Fs": {
+        server: {
+
+        }
+    },
     "HTTP": {
         lib: {
             call: 4,
@@ -125,6 +176,61 @@ var predefs = {
             put: 3
         }
     },
+    "Logger": {
+        lib: {
+            debug: 10000,
+            error: 10000,
+            info: 10000,
+            setLevel: 1,
+            setLevels: 1,
+            trace: 10000,
+            warn: 10000
+        }
+    },
+    "Meteor": {
+        client: {
+            defer: 1, // https://github.com/meteor/meteor/issues/2176
+            disconnect: 0,
+            loggingIn: 0,
+            loginWithPassword: 3,
+            loginWithToken: 1,
+            logout: 1,
+            logoutOtherClients: 1,
+            reconnect: 0,
+            status: 0,
+            subscribe: 10000
+        },
+        lib: {
+            absoluteUrl: 2,
+            apply: 4,
+            bindEnvironment: 2,
+            call: 10000,
+            clearInterval: 1,
+            clearTimeout: 1,
+            isClient: -1,
+            isServer: -1,
+            isCordova: -1,
+            methods: 1,
+            setInterval: 2,
+            setTimeout: 2,
+            settings: -1,
+            release: -1,
+            startup: 1,
+            user: 0,
+            userId: 0,
+            users: 0,
+            wrapAsync: 2
+        },
+        server: {
+            onConnection: 1,
+            publish: 2
+        }
+    },
+    "Npm": {
+        server: {
+            require: 1
+        }
+    },
     // docs.meteor.com
     "Random": {
         lib: {
@@ -132,10 +238,31 @@ var predefs = {
             id: 1
         }
     },
+    "Router": {
+        client: {
+            go: 3,
+            path: 2
+        },
+        lib: {
+            configure: 1,
+            current: 0,
+            route: 3,
+            routes: -1
+        }
+    },
     // docs.meteor.com
     "ServiceConfiguration": {
         lib: {
             configurations: -1
+        }
+    },
+    "Session": {
+        client: {
+            equals: 2,
+            get: 1,
+            set: 2,
+            setDefault: 2
+
         }
     },
     // https://github.com/TAPevents/tap-i18n/
@@ -158,6 +285,28 @@ var predefs = {
             setLanguage: 1
         }
     },
+    "Template": {
+        client: {
+            body: -1,
+            currentData: 0,
+            instance: 0,
+            parentData: 1,
+            registerHelper: 2
+        }
+    },
+    "Tracker": {
+        client: {
+            active: -1,
+            afterFlush: 1,
+            currentComputation: -1,
+            flush: 0,
+            nonreactive: 1,
+            onInvalidate: 1
+        },
+        lib: {
+            autorun: 2
+        }
+    },
     // https://github.com/percolatestudio/publish-counts
     "Counts": {
         client: {
@@ -167,6 +316,149 @@ var predefs = {
         server: {
             publish: 4,
             noWarnings: 0
+        }
+    },
+    "Facts": {
+        server: {
+            setUserIdFilter: 1
+        }
+    },
+    "UI": {
+        client: {
+            registerHelper: 2
+        }
+    },
+    // http://devdocs.io/underscore/
+    "_": {
+        lib: {
+            after: 2,
+            all: 3,
+            allKeys: 1,
+            any: 3,
+            before: 2,
+            bind: 10000,
+            bindAll: 10000,
+            chain: 1,
+            clone: 1,
+            collect: 3,
+            compact: 1,
+            compose: 10000,
+            constant: 1,
+            contains: 3,
+            countBy: 3,
+            create: 2,
+            debounce: 3,
+            defaults: 10000,
+            defer: 10000,
+            delay: 10000,
+            detect: 3,
+            difference: 10000,
+            drop: 2,
+            each: 3,
+            escape: 1,
+            every: 3,
+            extend: 10000,
+            extendOwn: 10000,
+            filter: 3,
+            find: 3,
+            findIndex: 3,
+            findKey: 3,
+            findLastIndex: 3,
+            findWhere: 2,
+            first: 2,
+            flatten: 2,
+            foldl: 4,
+            foldr: 4,
+            forEach: 3,
+            functions: 1,
+            groupBy: 3,
+            has: 2,
+            head: 2,
+            identity: 1,
+            includes: 3,
+            indexBy: 3,
+            indexOf: 3,
+            initial: 2,
+            inject: 4,
+            intersection: 10000,
+            invert: 1,
+            invoke: 10000,
+            isArguments: 1,
+            isArray: 1,
+            isBoolean: 1,
+            isDate: 1,
+            isElement: 1,
+            isEmpty: 1,
+            isEqual: 2,
+            isError: 1,
+            isFinite: 1,
+            isFunction: 1,
+            isMatch: 1,
+            isNaN: 1,
+            isNull: 1,
+            isNumber: 1,
+            isObject: 1,
+            isRegExp: 1,
+            isString: 1,
+            isUndefined: 1,
+            iteratee: 2,
+            keys: 1,
+            last: 2,
+            lastIndexOf: 3,
+            map: 3,
+            mapObject: 3,
+            matcher: 1,
+            matches: 1,
+            max: 3,
+            memoize: 2,
+            methods: 1,
+            min: 3,
+            mixin: 1,
+            negate: 1,
+            noConflict: 0,
+            noop: 0,
+            now: 0,
+            object: 2,
+            omit: 10000,
+            once: 1,
+            pairs: 1,
+            partial: 10000,
+            partition: 3,
+            pick: 10000,
+            pluck: 2,
+            property: 1,
+            propertyOf: 1,
+            random: 2,
+            range: 3,
+            reduce: 4,
+            reduceRight: 4,
+            reject: 3,
+            rest: 2,
+            result: 3,
+            sample: 2,
+            select: 3,
+            shuffle: 1,
+            size: 1,
+            some: 3,
+            sortBy: 3,
+            sortedIndex: 4,
+            tail: 2,
+            take: 2,
+            tap: 2,
+            template: 2,
+            throttle: 3,
+            times: 3,
+            toArray: 1,
+            unescape: 1,
+            union: 10000,
+            uniq: 3,
+            uniqId: 1,
+            unzip: 10000,
+            values: 1,
+            where: 2,
+            without: 10000,
+            wrap: 2,
+            zip: 10000
         }
     }
 };
@@ -356,12 +648,12 @@ function addDeclsForName(file, loc, name, value, decls) {
         if (debug) console.log(loc, "found decl", name, "= new", typ);
         if ((typ === "Mongo.Collection") || (typ === "Meteor.Collection")) {
             addAllFns(decls, loc, name, predefPrototypes["Mongo.Collection"]);
-            return;
+            arity = -1;
         } else {
             if (debug) console.log("new", typ);
         }
     } else if (value.type === "ObjectExpression") {
-		if (debug) console.log(loc, "found decl", name + ": Object");
+        if (debug) console.log(loc, "found decl", name + ": Object");
         value.properties.forEach(function(prop) {
             if (prop.key.type === "Identifier") {
                 var nameCompo = name + "." + prop.key.name;
@@ -371,7 +663,7 @@ function addDeclsForName(file, loc, name, value, decls) {
                 if (debug) console.log("object unsupported key", prop.key);
             }
         });
-        return;
+        arity = -1;
     } else {
         if (debug) console.log(loc, "found decl", name);
     }
